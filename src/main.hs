59940289439cc72cd4f7e8eabb8e4b0bfbda8757
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-02-08 19:03:52 +0100

    brake recursive parsing

--- src/main.hs
+++ src/main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Debug.Trace
+
 import Text.ParserCombinators.Parsec
 import qualified Text.ParserCombinators.Parsec.Token as Token
 import Text.ParserCombinators.Parsec.Language
@@ -26,25 +28,12 @@ data Expr = Plus Expr Expr
           | String
           deriving (Read, Show)
 
-class Pretty p where
-  ppr :: Int -> p -> Doc
-
-instance Pretty Expr where
-  ppr _ Zero = PP.text "0"
-  ppr _ String = PP.text "?"
-  --ppr p (Plus a b) =
-  --      PP.text "+" <+> ppr p a
-  --  <+> PP.text "+" <+> ppr p b
-
 toString :: Expr -> String
 toString (Plus l r) = "(+ " ++ toString l ++ " " ++ toString r ++ ")"
 toString x = show x
 
 toExpr :: String -> Expr
 toExpr x = read x :: Expr
-
-intToExpr :: Integer -> Expr
-intToExpr x = Num x
 
 lexer :: Token.TokenParser ()
 lexer = Token.makeTokenParser emptyDef
@@ -57,28 +46,34 @@ parseInfix = do
               exp <- psExp
               return exp
 
---psExp :: _ -> String
 psExp = do
             left <- psTerm
             exp <- psExpStar left
             return exp
 
+createExpr :: Char -> Expr -> Expr -> Expr
+createExpr '+' l r = Plus l r
+createExpr '-' l r = Sub l r
+createExpr '/' l r = Devide l r
+createExpr '*' l r = Mult l r
+createExpr _ l r = Zero
+
 --Parse calcul
-parseOperation :: String -> Expr -> Parser Expr
+parseOperation :: Char -> Expr -> Parser Expr
 parseOperation op l = do
-                            symbol op
+                            symbol [op]
                             r <- psFactor
-                            append <- psTermStar $ Plus l r
+                            append <- psTermStar $ createExpr op l r
                             return append
 
 psExpStar :: Expr -> Parser Expr
-psExpStar left = parseOperation "+" left
-                    <|> parseOperation "-" left
+psExpStar left = parseOperation '+' left
+                    <|> parseOperation '-' left
                     <|> return left
 
 psTermStar :: Expr -> Parser Expr
-psTermStar left = parseOperation "*" left
-                    <|> parseOperation "/" left
+psTermStar left = parseOperation '*' left
+                    <|> parseOperation '/' left
                     <|> return left
 
 psTerm = do
@@ -86,11 +81,11 @@ psTerm = do
             exp <- psTermStar left
             return exp
 
---Number conversion
+-- Parse all the stuffs
 psFactor = 
             (do {
                 obj <- number;
-                return (intToExpr obj)
+                return (Num obj)
             })
             <|>
             (do {
@@ -104,9 +99,6 @@ psFactor =
                 symbol ")";
                 return obj
             })
-
---eval :: String -> Maybe String
---eval t = return t
 
 convertI2S :: String -> Either ParseError Expr
 convertI2S x = parse parseInfix "i2s" x
